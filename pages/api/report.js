a1f5091e3d71770a0bc51a3237048206e68211e8
mdcarter <maxime.dehaye@gmail.com>
2021-10-28 10:08:02 +0200

    Add dynamic dates

--- pages/api/report.js
+++ pages/api/report.js
@@ -1,12 +1,16 @@
 import { Session } from 'linky'
+import { format, subDays } from 'date-fns'
+
+const dateFormat = 'yyyy-MM-dd'
+const daysBefore = 30
+
+const session = new Session({
+  accessToken: process.env.ACCESS_TOKEN,
+  refreshToken: process.env.REFRESH_TOKEN,
+  usagePointId: process.env.USAGE_POINT_ID
+})
 
 export default async function handler(_, res) {
-  const session = new Session({
-    accessToken: process.env.ACCESS_TOKEN,
-    refreshToken: process.env.REFRESH_TOKEN,
-    usagePointId: process.env.USAGE_POINT_ID
-  })
-
-  const result = await session.getDailyConsumption('2021-10-01', '2022-10-27')
+  const result = await session.getDailyConsumption(format(subDays(new Date(), daysBefore), dateFormat), format(new Date(), dateFormat))
   res.status(200).json(result)
 }
